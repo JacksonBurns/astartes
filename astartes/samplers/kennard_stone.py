cec76eecaf4539c2cc7aba905fe0698a66052c71
himaghna <himaghna@udel.edu>
2022-05-09 18:02:58 -0700

    Skeleton of KennardStone class

--- astartes/samplers/kennard_stone.py
+++ astartes/samplers/kennard_stone.py
@@ -1,15 +1,30 @@
-from kennard_stone import train_test_split
+from sampler import Sampler
 
 
-def kennard_stone(
-    X,
-    y=None,
-    test_size=None,
-    train_size=None,
-):
-    return train_test_split(
-        X,
-        y,
-        test_size=test_size,
-        train_size=train_size,
-    )
+class KennardStone(Sampler):
+    """
+    Implements the algorithm outlined in
+    Kennard, R. W., & Stone, L. A. (1969).
+    Computer Aided Design of Experiments. Technometrics, 11(1), 137–148.
+    https://doi.org/10.1080/00401706.1969.10490666
+    
+    """
+    def __init__(self, configs):
+        pass
+
+    def populate(self, X, y=None):
+        pass
+
+    def get_sample(self):
+        pass
+
+    def get_sample_id(self):
+        pass
+
+    def get_batch_sample(self, n_samples):
+        pass
+
+    def get_batch_sample_idx(self, n_samples):
+        pass
+
+
