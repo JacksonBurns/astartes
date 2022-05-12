12f1bda66a395724baf62d5b06209609fe188a27
JacksonBurns <JacksonBurns@users.noreply.github.com>
2022-05-12 12:06:24 -0400

    finish random sampler

--- astartes/samplers/random_split.py
+++ astartes/samplers/random_split.py
@@ -8,12 +8,16 @@ class Random(Sampler):
 
     def __init__(self, configs):
         self._split = False
+        self._random_state = configs.get('random_state', None)
+        self._shuffle = configs.get('shuffle', True)
         self._samples_idxs = []
 
-    def _ks_split(self):
+    def _rand_split(self):
         _, _, samples_idxs, spare_idx = train_test_split(
             self.X, list(range(len(self.X))),
             train_size=len(self.X)-1,
+            random_state=self._random_state,
+            shuffle=self._shuffle,
         )
         self._samples_idxs = samples_idxs + spare_idx
 
@@ -21,6 +25,6 @@ class Random(Sampler):
         if self._split:
             return self._samples_idxs.pop(0)
         else:
-            self._ks_split()
+            self._rand_split()
             self._split = True
             return self._get_next_sample_idx()
