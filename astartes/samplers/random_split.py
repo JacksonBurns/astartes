e3db18614c31a7e36b6c26ef75c82dcbc12ac756
JacksonBurns <JacksonBurns@users.noreply.github.com>
2022-05-11 23:51:22 -0400

    Update random_split.py

--- astartes/samplers/random_split.py
+++ astartes/samplers/random_split.py
@@ -1,20 +1,26 @@
 from astartes.samplers import Sampler
 
 # https://github.com/yu9824/kennard_stone
-from kennard_stone import train_test_split
+from sklearn.model_selection import train_test_split
 
 
 class Random(Sampler):
 
     def __init__(self, configs):
-        self._ks_split(self)
+        self._split = False
+        self._samples_idxs = []
 
     def _ks_split(self):
-        _, _, samples_idxs, _ = train_test_split(
-            self.X, [0]*len(self.X),
-            train_size=1.0,
+        _, _, samples_idxs, spare_idx = train_test_split(
+            self.X, list(range(len(self.X))),
+            train_size=len(self.X)-1,
         )
-        self._samples_idxs = samples_idxs
+        self._samples_idxs = samples_idxs + spare_idx
 
-    def get_next_sample_idx(self):
-        return self._res.pop(0)
+    def _get_next_sample_idx(self):
+        if self._split:
+            return self._samples_idxs.pop(0)
+        else:
+            self._ks_split()
+            self._split = True
+            return self._get_next_sample_idx()
