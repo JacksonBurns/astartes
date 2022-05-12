c06c4496896414cf0eebc161707f2c258e807d73
JacksonBurns <JacksonBurns@users.noreply.github.com>
2022-05-12 13:23:01 -0400

    remove debugging line

--- .github/workflows/coverage_helper.py
+++ .github/workflows/coverage_helper.py
@@ -3,8 +3,6 @@ import codecs
 with codecs.open("temp.txt", encoding='utf-8') as file:
     lines = file.readlines()
 
-print(lines)
-
 coverage = lines[-1].split()[-1].replace("%", "")
 
 print('Test coverage is', coverage, 'percent.')
