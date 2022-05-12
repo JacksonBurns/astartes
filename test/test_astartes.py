e86bc96306cc21197ff180a8a462d013229e80bc
JacksonBurns <JacksonBurns@users.noreply.github.com>
2022-05-12 16:12:03 -0400

    delete old test

--- test/test_astartes.py
+++ test/test_astartes.py
@@ -2,7 +2,9 @@ import os
 import sys
 import unittest
 
-# from astartes import astartes
+import numpy as np
+
+from astartes import train_test_split
 
 
 class Test_astartes(unittest.TestCase):
@@ -14,10 +16,22 @@ class Test_astartes(unittest.TestCase):
     def setUpClass(self):
         return
 
-    def test_placeholder(self):
+    def test_train_test_split(self):
         """
         """
-        self.assertTrue(1+1 == 2)
+        X_train, X_test, y_train, y_test = train_test_split(
+            np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+            np.array([10, 11, 12]),
+            test_size=0.2,
+            train_size=0.8,
+            splitter='random',
+            hopts={
+                'shuffle': True,
+                'random_state': 42,
+            }
+        )
+        for elt, ans in zip(X_train.flatten(), [4, 5, 6, 7, 8, 9]):
+            self.assertEqual(elt, ans)
 
 
 if __name__ == '__main__':
