4faf00029f3baf29ba1fed2fedf5e863b3dc550c
Jackson Burns <jwburns@mit.edu>
2023-02-09 11:25:27 -0500

    change imports to make error for optional install raise at right time

--- astartes/__init__.py
+++ astartes/__init__.py
@@ -1,4 +1,11 @@
+# convenience import to enable 'from astartes import train_test_split
 from .astartes import train_test_split
+
+# DO NOT do this:
+# from .molecules import train_test_split_molecules
+# on installations without the optional molecules install, this would cause
+# an exception
+
 from .astartes import IMPLEMENTED_SAMPLERS
 
 
