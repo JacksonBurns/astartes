2111a21951510f5019540619ae10394b15e3e506
Jackson Burns <jwburns@mit.edu>
2023-02-09 14:52:47 -0500

    additional test refactoring

--- test/utils/test_exceptions.py
+++ test/utils/test_exceptions.py
@@ -1,1 +1,30 @@
+import os
+import sys
+import unittest
 
+import numpy as np
+
+from astartes import train_test_split
+
+from astartes.samplers import (
+    IMPLEMENTED_UNSUPERVISED_SAMPLERS,
+    IMPLEMENTED_SUPERVISED_SAMPLERS,
+)
+
+
+class Test_exceptions(unittest.TestCase):
+    """
+    Test that exceptions are raised when appropriate.
+    """
+
+    @classmethod
+    def setUpClass(self):
+        return
+
+    def test_train_test_split(self):
+        """ """
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
