14dd33eb3dc2bcfb1508545f18da09984b848f23
Jackson Burns <jwburns@mit.edu>
2023-02-11 17:51:10 -0500

    add tests for kmeans sampler (and the sampler)

--- astartes/samplers/extrapolation/kmeans.py
+++ astartes/samplers/extrapolation/kmeans.py
@@ -1,8 +1,10 @@
 from astartes.samplers import AbstractSampler
 
 from math import floor
+from collections import Counter
 
 from sklearn.cluster import KMeans as sk_KMeans
+import numpy as np
 
 
 class KMeans(AbstractSampler):
@@ -10,12 +12,47 @@ class KMeans(AbstractSampler):
         super().__init__(*args)
 
     def _sample(self):
+        """Implements the K-Means sampler to identify clusters."""
+        # use the sklearn kmeans model
         kmeanModel = sk_KMeans(
             n_clusters=self.get_config("n_clusters", floor(len(self.X) * 0.1) + 1),
-            n_init=self.get_config("n_init", 10),
+            n_init=self.get_config("n_init", "auto"),
             random_state=self.get_config("random_state", None),
         ).fit(self.X)
-        print(kmeanModel.labels_)
-        # output list of two-tuples with index and cluster, or find a better way
-        # sort in order of smallest to largest cluster, use that sort to sort the labels as well
-        pass
+        self._samples_clusters = kmeanModel.labels_
+
+        # need to sort labels and clusters in order of smallest cluster to largest
+        # start by counting the number of elements in each cluster
+        cluster_counter = Counter(kmeanModel.labels_)
+
+        # create an integer array that stores the indices as they are now, which we
+        # will sort alongside the labels based on the number of elements in each
+        # cluster (effectively tracking the mapping between unsorted and sorted)
+        samples_idxs = np.array(range(len(self.X)), dtype=int)
+
+        # workhorse - this and some lines above/below could be made into a private
+        # method in AbstractSampler
+        sorted_idxs = []
+        sorted_cluster_counter = {}
+        for label, sample_idx in sorted(
+            zip(
+                kmeanModel.labels_, samples_idxs
+            ),  # iterate indices and labels simultaneously
+            key=lambda pair: cluster_counter[
+                pair[0]
+            ],  # use the number of elements in the cluster for sorting
+        ):
+            print(label, sample_idx)
+            sorted_idxs.append(sample_idx)
+            if label not in sorted_cluster_counter:
+                sorted_cluster_counter[label] = 1
+            else:
+                sorted_cluster_counter[label] += 1
+
+        # can't use np.argsort because it does not allow for a custom comparison key
+        # and will instead sort by the value of the cluster labels (wrong!)
+
+        print(self.X)
+        print(sorted_idxs, kmeanModel.labels_[sorted_idxs], kmeanModel.labels_)
+        self._samples_idxs = sorted_idxs
+        self._sorted_cluster_counter = sorted_cluster_counter
---
+++ test/samplers/test_kmeans.py
@@ -0,0 +1,149 @@
+import unittest
+
+import numpy as np
+
+from astartes.samplers import KMeans
+from astartes import train_test_split
+
+
+class Test_kmeans(unittest.TestCase):
+    """
+    Test the various functionalities of kmeans.
+    """
+
+    @classmethod
+    def setUpClass(self):
+        self.X = np.array(
+            [
+                [0, 0, 0, 0, 0],
+                [1, 0, 0, 0, 0],
+                [1, 1, 0, 0, 0],
+                [1, 1, 1, 0, 0],
+                [1, 1, 1, 1, 0],
+            ]
+        )
+        self.y = np.array([1, 2, 3, 4, 5])
+        self.labels = np.array(
+            [
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+            ]
+        )
+
+    def test_kmeans_sampling(self):
+        """Use kmeans in the train_test_split and verify results."""
+        with self.assertWarns(RuntimeWarning):
+            (
+                X_train,
+                X_test,
+                y_train,
+                y_test,
+                labels_train,
+                labels_test,
+                clusters_train,
+                clusters_test,
+            ) = train_test_split(
+                self.X,
+                self.y,
+                labels=self.labels,
+                test_size=0.75,
+                train_size=0.25,
+                sampler="kmeans",
+                hopts={
+                    "n_clusters": 2,
+                    "random_state": 42,
+                },
+            )
+        # test that the known arrays equal the result from above
+        self.assertIsNone(
+            np.testing.assert_array_equal(
+                X_train,
+                np.array([[0, 0, 0, 0, 0], [1, 0, 0, 0, 0], [1, 1, 0, 0, 0]]),
+            ),
+            "Train X incorrect.",
+        )
+        self.assertIsNone(
+            np.testing.assert_array_equal(
+                X_test,
+                np.array([[1, 1, 1, 0, 0], [1, 1, 1, 1, 0]]),
+            ),
+            "Test X incorrect.",
+        )
+        self.assertIsNone(
+            np.testing.assert_array_equal(
+                y_train,
+                np.array([1, 2, 3]),
+            ),
+            "Train y incorrect.",
+        )
+        self.assertIsNone(
+            np.testing.assert_array_equal(
+                y_test,
+                np.array([4, 5]),
+            ),
+            "Test y incorrect.",
+        )
+        self.assertIsNone(
+            np.testing.assert_array_equal(
+                labels_train,
+                np.array(["one", "two", "three"]),
+            ),
+            "Train labels incorrect.",
+        )
+        self.assertIsNone(
+            np.testing.assert_array_equal(
+                labels_test,
+                np.array(["four", "five"]),
+            ),
+            "Test labels incorrect.",
+        )
+        self.assertIsNone(
+            np.testing.assert_array_equal(
+                clusters_train,
+                np.array([1, 1, 1]),
+            ),
+            "Train clusters incorrect.",
+        )
+        self.assertIsNone(
+            np.testing.assert_array_equal(
+                clusters_test,
+                np.array([0, 0]),
+            ),
+            "Test clusters incorrect.",
+        )
+
+    def test_kmeans(self):
+        """Directly instantiate and test KMeans."""
+        kmeans_instance = KMeans(
+            self.X,
+            self.y,
+            self.labels,
+            {
+                "n_clusters": 2,
+                "random_state": 42,
+            },
+        )
+        self.assertIsInstance(
+            kmeans_instance,
+            KMeans,
+            "Failed instantiation.",
+        )
+        self.assertTrue(
+            len(kmeans_instance.get_clusters()),
+            "Clusters not set.",
+        )
+        self.assertTrue(
+            len(kmeans_instance.get_sorted_cluster_counter()),
+            "Sorted cluster Counter not found.",
+        )
+        self.assertTrue(
+            len(kmeans_instance._samples_idxs),
+            "Sample indices not set.",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
