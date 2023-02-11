321ee27a32d60b565ea9d8fb8d3da7e8b3a8ceb6
Jackson Burns <jwburns@mit.edu>
2023-02-11 17:50:45 -0500

    type fixes, doc updates, function simplification

--- astartes/main.py
+++ astartes/main.py
@@ -1,5 +1,6 @@
 from difflib import get_close_matches
 from math import floor
+from warnings import warn
 
 import numpy as np
 
@@ -47,10 +48,17 @@ def train_test_split(
     Returns:
         np.array: Training and test data split into arrays.
     """
-    if sampler not in ALL_SAMPLERS:
+    sampler = sampler.lower()
+    if sampler == "random":
+        sampler_class = Random
+    elif sampler == "kennard_stone":
+        sampler_class = KennardStone
+    elif sampler == "kmeans":
+        sampler_class = KMeans
+    else:
         possiblity = get_close_matches(
             sampler,
-            IMPLEMENTED_EXTRAPOLATION_SAMPLERS + IMPLEMENTED_INTERPOLATION_SAMPLERS,
+            ALL_SAMPLERS,
             n=1,
         )
         addendum = (
@@ -62,128 +70,132 @@ def train_test_split(
             "Sampler {:s} has not been implemented.".format(sampler) + addendum
         )
 
-    if sampler in IMPLEMENTED_EXTRAPOLATION_SAMPLERS:
-        return _extrapolative_sampling(
-            X,
-            y,
-            labels,
-            test_size,
-            train_size,
-            sampler,
-            hopts,
-            return_indices,
-        )
-    else:
-        return _interpolative_sampling(
-            X,
-            y,
-            labels,
-            test_size,
-            train_size,
-            sampler,
-            hopts,
-            return_indices,
-        )
-
-
-def _extrapolative_sampling(
-    X: np.array,
-    y: np.array = None,
-    labels: np.array = None,
-    test_size: float = None,
-    train_size: float = 0.75,
-    sampler: str = "random",
-    hopts: dict = {},
-    return_indices: bool = False,
-):
-    # validation for extrapolative sampling
-    msg = None
-    if labels is None:  # labeled samplers need labels (but not vice versa!)
-        msg = """Sampler {:s} requires labeled data, but labels were not provided.""".format(
-            sampler
-        )
-    elif len(X) != np.size(labels):  # size of data must match number of labels
-        msg = """First dimension of X and labels must match (got X dim={:d} and labels dim={:d})""".format(
-            len(X),
-            np.size(labels),
-        )
-    if msg:
-        raise InvalidAstartesConfigurationError(msg)
-
-    sampler_class = None
-    if sampler == "kmeans":
-        sampler_class = KMeans
-
-    sampler_instance = sampler_class(X, y, labels, hopts)
-
-    if test_size is None:
-        test_size = 1.0 - train_size
-
-    n_test_samples = floor(len(X) * test_size)
-
-    # can't break up clusters
-    # warn the user that the exact split they asked for was not possible
-    # this is what they get isntead
-    # we exoect that supervised samplers will return lists of tuples
-    # where tuple(1) is the cluster id
-
-    cluster_counter = sampler_instance.get_cluster_counter()
-    test_idxs, train_idxs = [], []
-    print(cluster_counter)
-    for cluster_idx, n_samples in cluster_counter.items():
-        print(n_samples)
-        # assemble test first
-        if (len(test_idxs) + n_samples) < n_test_samples:  # will not overfill
-            test_idxs.append(sampler_instance.get_sample_idxs(n_samples))
-    else:  # then balance goes into train
-        train_idxs.append(sampler_instance.get_sample_idxs(n_samples))
-    # TODO: get the clusters in order, get the other array s(abstract into a helper for both types of splitting) and then abstract the actual result checker and clal it from both
-
-
-def _interpolative_sampling(
-    X: np.array,
-    y: np.array = None,
-    labels: np.array = None,
-    test_size: float = None,
-    train_size: float = 0.75,
-    sampler: str = "random",
-    hopts: dict = {},
-    return_indices: bool = False,
-):
-    sampler_class = None
-    if sampler == "random":
-        sampler_class = Random
-    elif sampler == "kennard_stone":
-        sampler_class = KennardStone
-
     sampler_instance = sampler_class(X, y, labels, hopts)
 
     if test_size is not None:
         train_size = 1.0 - test_size
 
-    n_train_samples = floor(len(X) * train_size)
-    # TODO: warm the user about what the actual split came out to be
+    if sampler in IMPLEMENTED_EXTRAPOLATION_SAMPLERS:
+        return _extrapolative_sampling(
+            sampler_instance,
+            test_size,
+            train_size,
+            return_indices,
+        )
+    else:
+        return _interpolative_sampling(
+            sampler_instance,
+            test_size,
+            train_size,
+            return_indices,
+        )
+
+
+def _extrapolative_sampling(
+    sampler_instance,
+    test_size,
+    train_size,
+    return_indices,
+):
+    # validation for extrapolative sampling
+    n_test_samples = floor(len(sampler_instance.X) * test_size)
+
+    # can't break up clusters
+    cluster_counter = sampler_instance.get_sorted_cluster_counter()
+    test_idxs, train_idxs = np.array([], dtype=int), np.array([], dtype=int)
+    for cluster_idx, cluster_length in cluster_counter.items():
+        # assemble test first
+        print(cluster_idx, len(test_idxs), cluster_length, n_test_samples)
+        if (len(test_idxs) + cluster_length) <= n_test_samples:  # will not overfill
+            test_idxs = np.append(
+                test_idxs, sampler_instance.get_sample_idxs(cluster_length)
+            )
+        else:  # then balance goes into train
+            train_idxs = np.append(
+                train_idxs, sampler_instance.get_sample_idxs(cluster_length)
+            )
+        print(test_idxs, train_idxs)
+    _check_actual_split(train_idxs, test_idxs, train_size, test_size)
+    return _return_helper(sampler_instance, train_idxs, test_idxs, return_indices)
+
+
+def _interpolative_sampling(
+    sampler_instance,
+    test_size,
+    train_size,
+    return_indices,
+):
+
+    n_train_samples = floor(len(sampler_instance.X) * train_size)
 
     train_idxs = sampler_instance.get_sample_idxs(n_train_samples)
-    test_idxs = sampler_instance.get_sample_idxs(len(X) - n_train_samples)
+    test_idxs = sampler_instance.get_sample_idxs(
+        len(sampler_instance.X) - n_train_samples
+    )
+    _check_actual_split(train_idxs, test_idxs, train_size, test_size)
+    return _return_helper(sampler_instance, train_idxs, test_idxs, return_indices)
+
+
+def _return_helper(
+    sampler_instance,
+    train_idxs,
+    test_idxs,
+    return_indices,
+):
     if return_indices:
         return train_idxs, test_idxs
-    else:
-        out = []
-        X_train = np.array([X[i] for i in train_idxs], dtype=object)
-        out.append(X_train)
-        X_test = np.array([X[i] for i in test_idxs], dtype=object)
-        out.append(X_test)
+    out = []
+    X_train = sampler_instance.X[train_idxs]
+    out.append(X_train)
+    X_test = sampler_instance.X[test_idxs]
+    out.append(X_test)
 
-        if y is not None:
-            y_train = np.array([y[i] for i in train_idxs], dtype=object)
-            out.append(y_train)
-            y_test = np.array([y[i] for i in test_idxs], dtype=object)
-            out.append(y_test)
-        if labels is not None:
-            labels_train = np.array([labels[i] for i in train_idxs], dtype=object)
-            out.append(labels_train)
-            labels_test = np.array([labels[i] for i in test_idxs], dtype=object)
-            out.append(labels_test)
+    if sampler_instance.y is not None:
+        y_train = np.array([sampler_instance.y[i] for i in train_idxs], dtype=object)
+        out.append(y_train)
+        y_test = np.array([sampler_instance.y[i] for i in test_idxs], dtype=object)
+        out.append(y_test)
+    if sampler_instance.labels is not None:
+        labels_train = np.array(
+            [sampler_instance.labels[i] for i in train_idxs], dtype=object
+        )
+        out.append(labels_train)
+        labels_test = np.array(
+            [sampler_instance.labels[i] for i in test_idxs], dtype=object
+        )
+        out.append(labels_test)
+    if len(sampler_instance.get_clusters()):  # true when the list has been filled
+        clusters_train = np.array(
+            [sampler_instance.get_clusters()[i] for i in train_idxs], dtype=object
+        )
+        out.append(clusters_train)
+        clusters_test = np.array(
+            [sampler_instance.get_clusters()[i] for i in test_idxs], dtype=object
+        )
+        out.append(clusters_test)
 
-        return (*out,)
+    return (*out,)
+
+
+def _check_actual_split(train_idxs, test_idxs, train_size, test_size):
+    # print(train_idxs, test_idxs, train_size, test_size)
+    actual_train_size = round(len(train_idxs) / (len(test_idxs) + len(train_idxs)), 3)
+    requested_train_size = round(train_size, 3)
+    actual_test_size = round(1.0 - actual_train_size, 3)
+    requested_test_size = round(test_size, 3)
+    msg = ""
+    if actual_train_size != requested_train_size:
+        msg += "Requested train size of {:.2f}, got {:.2f}. ".format(
+            requested_train_size,
+            actual_train_size,
+        )
+    if actual_test_size != requested_test_size:
+        msg += "Requested test size of {:.2f}, got {:.2f}. ".format(
+            requested_test_size,
+            actual_test_size,
+        )
+    if msg:
+        warn(
+            "Actual train/test split differs from requested size. " + msg,
+            RuntimeWarning,
+        )
