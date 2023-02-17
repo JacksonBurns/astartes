ea7b53ebe2e721e700124f9cba784e50784a6d1e
JacksonBurns <33505528+JacksonBurns@users.noreply.github.com>
2023-02-17 16:34:34 -0500

    add train_val_test_split_molecules and tests

--- test/test_molecules.py
+++ test/test_molecules.py
@@ -5,7 +5,10 @@ import warnings
 
 import numpy as np
 
-from astartes.molecules import train_test_split_molecules
+from astartes.molecules import (
+    train_test_split_molecules,
+    train_val_test_split_molecules,
+)
 from astartes.samplers import (
     IMPLEMENTED_EXTRAPOLATION_SAMPLERS,
     IMPLEMENTED_INTERPOLATION_SAMPLERS,
@@ -42,10 +45,22 @@ class Test_molecules(unittest.TestCase):
             tts = train_test_split_molecules(
                 self.X,
                 self.y,
-                0.2,
+                train_size=0.2,
                 sampler=sampler,
                 fprints_hopts={"n_bits": 100},
             )
+
+    def test_validation_split_molecules(self):
+        """ """
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            for sampler in IMPLEMENTED_EXTRAPOLATION_SAMPLERS:
+                tts = train_val_test_split_molecules(
+                    self.X,
+                    self.y,
+                    sampler=sampler,
+                    fprints_hopts={"n_bits": 100},
+                )
 
     def test_fingerprints(self):
         for fprint in [
@@ -57,7 +72,7 @@ class Test_molecules(unittest.TestCase):
                 tts = train_test_split_molecules(
                     self.X,
                     self.y,
-                    0.2,
+                    test_size=0.2,
                     sampler="random",
                     fingerprint=fprint,
                 )
@@ -66,7 +81,7 @@ class Test_molecules(unittest.TestCase):
         tts = train_test_split_molecules(
             self.X,
             self.y,
-            0.2,
+            train_size=0.2,
             sampler="random",
             fingerprint="topological_fingerprint",
             fprints_hopts={
@@ -106,7 +121,7 @@ class Test_molecules(unittest.TestCase):
         tts = train_test_split_molecules(
             self.X,
             self.y,
-            0.2,
+            train_size=0.2,
             fingerprint="topological_fingerprint",
             fprints_hopts={
                 "minPath": 2,
