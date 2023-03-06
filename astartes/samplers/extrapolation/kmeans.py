130a761c58722e86da720487fc01b6592da34e1a
Kevin Spiekermann <kspieker@mit.edu>
2023-03-06 09:13:03 -0500

    Allow arguments for KMeans to be accessible to users

--- astartes/samplers/extrapolation/kmeans.py
+++ astartes/samplers/extrapolation/kmeans.py
@@ -14,7 +14,13 @@ class KMeans(AbstractSampler):
         # use the sklearn kmeans model
         kmeanModel = sk_KMeans(
             n_clusters=self.get_config("n_clusters", floor(len(self.X) * 0.1) + 1),
+            init=self.get_config("init", "k-means++"),
             n_init=self.get_config("n_init", 1),
+            max_iter=self.get_config("max_iter", 300),
+            tol=self.get_config("tol", 1e-4),
+            verbose=self.get_config("verbose", 0),
             random_state=self.get_config("random_state", None),
+            copy_x=self.get_config("copy_x", True),
+            algorithm=self.get_config("algorithm", "lloyd"),
         ).fit(self.X)
         self._samples_clusters = kmeanModel.labels_
