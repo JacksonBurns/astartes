9f545036ee94c15ad0546df2d4234dc8cac472a8
Kevin Spiekermann <kspieker@mit.edu>
2023-03-06 16:45:19 -0500

    Incorporate helpful feedback from Jackson
     to support multiple versions of python

--- astartes/samplers/extrapolation/kmeans.py
+++ astartes/samplers/extrapolation/kmeans.py
@@ -12,15 +12,18 @@ class KMeans(AbstractSampler):
     def _sample(self):
         """Implements the K-Means sampler to identify clusters."""
         # use the sklearn kmeans model
-        kmeanModel = sk_KMeans(
-            n_clusters=self.get_config("n_clusters", floor(len(self.X) * 0.1) + 1),
-            init=self.get_config("init", "k-means++"),
-            n_init=self.get_config("n_init", 1),
-            max_iter=self.get_config("max_iter", 300),
-            tol=self.get_config("tol", 1e-4),
-            verbose=self.get_config("verbose", 0),
-            random_state=self.get_config("random_state", None),
-            copy_x=self.get_config("copy_x", True),
-            algorithm=self.get_config("algorithm", "lloyd"),
-        ).fit(self.X)
+        default_kwargs = {
+            "n_clusters": self.get_config("n_clusters", floor(len(self.X) * 0.1) + 1),
+            "init": self.get_config("init", "k-means++"),
+            "n_init": self.get_config("n_init", 1),
+            "max_iter": self.get_config("max_iter", 300),
+            "tol": self.get_config("tol", 1e-4),
+            "verbose": self.get_config("verbose", 0),
+            "random_state": self.get_config("random_state", None),
+            "copy_x": self.get_config("copy_x", True),
+        }
+        if self.get_config("algorithm", False):
+            default_kwargs["algorithm"] = self.get_config("algorithm")
+
+        kmeanModel = sk_KMeans(**default_kwargs).fit(self.X)
         self._samples_clusters = kmeanModel.labels_
