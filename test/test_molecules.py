5b0b58b9c5f421e1f7fa53eeac7f998e75903325
Jackson Burns <jwburns@mit.edu>
2023-03-23 23:33:44 -0400

    add test for passing rdkit objects directly

--- test/test_molecules.py
+++ test/test_molecules.py
@@ -4,6 +4,7 @@ import unittest
 import warnings
 
 import numpy as np
+from rdkit import Chem
 
 from astartes.molecules import (
     train_test_split_molecules,
@@ -36,6 +37,11 @@ class Test_molecules(unittest.TestCase):
         self.X = np.array(qm9_smiles_short)
         self.y = np.array(list(range(len(qm9_smiles_short))))
 
+        molecule_array = []
+        for smile in qm9_smiles_short:
+            molecule_array.append(Chem.MolFromSmiles(smile))
+        self.molecules = np.array(molecule_array)
+
         self.X_long = np.array(qm9_smiles_full)
         self.y_long = np.array(list(range(len(qm9_smiles_full))))
 
@@ -47,6 +53,27 @@ class Test_molecules(unittest.TestCase):
                 self.y,
                 train_size=0.2,
                 sampler=sampler,
+                fprints_hopts={"n_bits": 100},
+            )
+
+    def test_molecules_with_rdkit(self):
+        """Try train_test_split molecules, every sampler, passing rdkit objects."""
+        for sampler in IMPLEMENTED_INTERPOLATION_SAMPLERS:
+            tts = train_test_split_molecules(
+                self.molecules,
+                self.y,
+                train_size=0.2,
+                sampler=sampler,
+                fprints_hopts={"n_bits": 100},
+            )
+
+    def test_molecules_with_troublesome_smiles(self):
+        """Helpful errors when rdkit graphs can't be featurized."""
+        with self.assertRaises(RuntimeError):
+            tts = train_test_split_molecules(
+                np.array(["Nc1ncnc2n(cnc12)[C@@H]3O[C@H](CN=[N]=N)[C@@H](O)[C@H]3O"]),
+                train_size=0.2,
+                sampler="random",
                 fprints_hopts={"n_bits": 100},
             )
 
