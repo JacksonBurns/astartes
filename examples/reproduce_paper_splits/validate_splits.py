8eb99939cfb4b19b168104b346423d1c61da6682
Jackson Burns <jwburns@mit.edu>
2023-04-17 10:48:13 -0400

    add better info for debugging failed runs in all cases

--- examples/reproduce_paper_splits/validate_splits.py
+++ examples/reproduce_paper_splits/validate_splits.py
@@ -37,18 +37,39 @@ for reference, new in tests_dict.items():
         new_splits = pkl.load(f)
     for split_idx in range(5):
         for set_idx in range(3):
-            try:
-                np.testing.assert_array_equal(
-                    reference_splits[split_idx][set_idx],
-                    new_splits[split_idx][set_idx],
-                    "Failed {:s} split {:d} on {:s} set.".format(
-                        new,
-                        split_idx,
-                        split_names[set_idx],
-                    ),
+            reference_length = len(reference_splits[split_idx][set_idx])
+            new_length = len(new_splits[split_idx][set_idx])
+            shared_indexes = np.intersect1d(
+                reference_splits[split_idx][set_idx], new_splits[split_idx][set_idx]
+            )
+            largest_split = max(reference_length, new_length)
+            shared_percent = len(shared_indexes) / largest_split
+            print(
+                "\n{:s} split {:d} on {:s} set.".format(
+                    new,
+                    split_idx,
+                    split_names[set_idx],
                 )
-            except AssertionError as ae:
-                print(ae)
+            )
+            print(
+                "Dynamic size: {:d} Reference size: {:d}".format(
+                    new_length, reference_length
+                )
+            )
+            print(
+                "Dynamically generated and reference split shared {:.4f}% of indexes.".format(
+                    shared_percent * 100
+                )
+            )
+            are_eq = np.array_equal(
+                reference_splits[split_idx][set_idx],
+                new_splits[split_idx][set_idx],
+            )
+            print(
+                "Are arrays exactly equal: ",
+                are_eq,
+            )
+            if not are_eq:
                 fail = True
 if fail:
     raise RuntimeError("Regression testing failed, see above output.")
