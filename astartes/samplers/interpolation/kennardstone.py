ea9b7c813e730ad7345895b2b909dac44699d481
Jackson Burns <jwburns@mit.edu>
2023-06-05 11:13:39 -0400

    performance optimizations in `kennardstone.py` part 1
    use np.delete carefully to track which indices are selected
    above change allows using argmax and min instead of nan- prefixed, which is also faster

--- astartes/samplers/interpolation/kennardstone.py
+++ astartes/samplers/interpolation/kennardstone.py
@@ -2,7 +2,6 @@ import numpy as np
 from scipy.spatial.distance import pdist, squareform
 
 from astartes.samplers import AbstractSampler
-from astartes.utils.exceptions import InvalidConfigurationError
 
 
 class KennardStone(AbstractSampler):
@@ -20,31 +19,40 @@ class KennardStone(AbstractSampler):
         X_dist = pdist(self.X, metric=distance_metric)
 
         ks_distance = squareform(X_dist)
+        # when searching for max distance, disregard self
         np.fill_diagonal(ks_distance, -np.inf)
+
+        # delete from distance as we go, keep this array to keep track of idxs
+        ks_idxs = np.arange(n_samples)
 
         # get the row/col of maximum (greatest distance)
         max_idx = np.nanargmax(ks_distance)
         max_coords = np.unravel_index(max_idx, ks_distance.shape)
 
-        # -np.inf these points to trick numpy later on
-        ks_distance[max_coords[0], :] = -np.inf
-        ks_distance[max_coords[1], :] = -np.inf
+        # delete these rows so we don't select them again
+        ks_distance = np.delete(
+            ks_distance,
+            max_coords,
+            axis=0,
+        )
+        # delete from the index list to track deletions in distance matrix
+        ks_idxs = np.delete(ks_idxs, max_coords)
 
         # list of indices which have been selected, for use in the below loop
-        already_selected = list()
-        already_selected.append(max_coords[0])
-        already_selected.append(max_coords[1])
+        already_selected = list(max_coords)
 
         # iterate through the rest
         for _ in range(n_samples - 2):
             # find the next sample with the largest minimum distance to any sample already selected
-            # get out the columns for the data already selected
-            select_ks_distance = ks_distance[:, already_selected]
-            # find which member of the selected data each of the unselected data is closest to
-            min_distances_vals = np.nanmin(select_ks_distance, axis=1)
-            # pick the largest of those values
-            max_min_idx = np.nanargmax(min_distances_vals)
-            ks_distance[max_min_idx, :] = -np.inf
-            already_selected.append(max_min_idx)
+            max_min_idx = np.argmax(
+                np.min(
+                    ks_distance[:, already_selected],
+                    axis=1,
+                )  # find which member of the selected data each of the unselected data is closest to
+            )  # pick the largest of those values
+            # add to the selected, remove from index tracker and distance matrix
+            ks_distance = np.delete(ks_distance, max_min_idx, axis=0)
+            already_selected.append(ks_idxs[max_min_idx])
+            ks_idxs = np.delete(ks_idxs, max_min_idx)
 
         self._samples_idxs = np.array(already_selected, dtype=int)
