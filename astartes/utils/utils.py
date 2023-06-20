00b161e72b620bffb6149ea6cb1ccfcd4d5613ee
Kevin Spiekermann <kspieker@mit.edu>
2023-06-20 09:27:53 -0400

    Move function to utils folder

--- astartes/utils/utils.py
+++ astartes/utils/utils.py
@@ -1,77 +1,18 @@
-"""
-Draft script to train a simple sklearn model 
-and output the results that could be used to create 
-a table similar to what is from the paper.
-
-"""
-
-from pprint import pprint
-
-import numpy as np
-import pandas as pd
-from rdkit import Chem, DataStructs
-from rdkit.Chem import AllChem
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-from sklearn.svm import LinearSVR
 
 from astartes import train_val_test_split
 
-# read in the data
-CSV_PATH = '../barrier_prediction_with_RDB7/ccsdtf12_dz.csv'
-df = pd.read_csv(CSV_PATH)
-print(df.shape)
 
-# helper function to featurize the data with 2048 morgan fingerprint
-# https://github.com/chemprop/chemprop/blob/master/chemprop/features/features_generators.py
-MORGAN_RADIUS = 2
-MORGAN_NUM_BITS = 2048
-def morgan_counts_features_generator(mol,
-                                     radius= MORGAN_RADIUS,
-                                     num_bits= MORGAN_NUM_BITS):
-    """
-    Generates a counts-based Morgan fingerprint for a molecule.
-    :param mol: A molecule (i.e., either a SMILES or an RDKit molecule).
-    :param radius: Morgan fingerprint radius.
-    :param num_bits: Number of bits in Morgan fingerprint.
-    :return: A 1D numpy array containing the counts-based Morgan fingerprint.
-    """
-    mol = Chem.MolFromSmiles(mol) if type(mol) == str else mol
-    features_vec = AllChem.GetHashedMorganFingerprint(mol, radius, nBits=num_bits)
-    features = np.zeros((1,))
-    DataStructs.ConvertToNumpyArray(features_vec, features)
-
-    return features
-
-# create X and y
-params = Chem.SmilesParserParams()
-params.removeHs = False
-
-X = np.zeros((len(df), 2048*2))
-for i, row in df.iterrows():
-    rsmi, psmi = row.rsmi, row.psmi
-
-    rmol = Chem.MolFromSmiles(rsmi, params)
-    r_morgan = morgan_counts_features_generator(rmol)
-
-    pmol = Chem.MolFromSmiles(psmi, params)
-    p_morgan = morgan_counts_features_generator(pmol)
-
-    X[i, :] = np.concatenate((r_morgan,
-                              p_morgan - r_morgan),
-                             axis=0)
-
-y = df.dE0.values
-
-def produce_table(sklearn_model,
-                  X,
-                  y,
-                  samplers=["random"],
-                  random_state=0,
-                  sampler_hopts={},
-                  train_size=0.8,
-                  val_size=0.1,
-                  test_size=0.1,
-                  ):
+def generate_regression_results_dict(sklearn_model,
+                                     X,
+                                     y,
+                                     samplers=["random"],
+                                     random_state=0,
+                                     samplers_hopts={},
+                                     train_size=0.8,
+                                     val_size=0.1,
+                                     test_size=0.1,
+                                     ):
     """
     Helper function to train a sklearn model using the provided data
     and provided sampler types.
@@ -83,10 +24,9 @@ def produce_table(sklearn_model,
         val_size (float, optional): Fraction of dataset to use in validation set. Defaults to 0.1.
         test_size (float, optional): Fraction of dataset to use in test set. Defaults to 0.1.
         random_state (int, optional): The random seed used throughout astartes.
-        sampler_hopts (dict, optional): Hyperparameters for the sampler used above.
-                                        Should be a dictionary of dictionaries with the keys specifying
-                                        the sampler and the values being another dictionary with the 
-                                        hyperparameters. Defaults to {}.
+        samplers_hopts (dict, optional): Should be a dictionary of dictionaries with the keys specifying
+                                         the sampler and the values being another dictionary with the 
+                                         corresponding hyperparameters. Defaults to {}.
 
     Returns:
         dict: nested dictionary with the format of 
@@ -128,14 +68,14 @@ def produce_table(sklearn_model,
 
         # obtain indices
         _,_,_, train_indices, val_indices, test_indices = train_val_test_split(X,
-                                                                        train_size=train_size,
-                                                                        val_size=val_size,
-                                                                        test_size=test_size,
-                                                                        sampler=sampler,
-                                                                        random_state=random_state,
-                                                                        hopts=sampler_hopts.get(sampler, dict()),
-                                                                        return_indices=True,
-                                                                        )
+                                                                               train_size=train_size,
+                                                                               val_size=val_size,
+                                                                               test_size=test_size,
+                                                                               sampler=sampler,
+                                                                               random_state=random_state,
+                                                                               hopts=samplers_hopts.get(sampler, dict()),
+                                                                               return_indices=True,
+                                                                               )
 
         # create data splits
         X_train = X[train_indices]
@@ -189,10 +129,3 @@ def produce_table(sklearn_model,
         final_dict[sampler] = error_dict
 
     return final_dict
-
-
-# use default hyperparameters
-sklearn_model = LinearSVR()
-
-final_dict = produce_table(sklearn_model, X, y)
-pprint(final_dict)
