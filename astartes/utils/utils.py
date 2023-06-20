38420c615d289a8608b1d7e8f91055efb0c5adc1
Kevin Spiekermann <kspieker@mit.edu>
2023-06-20 09:36:56 -0400

    Apply black formatting

--- astartes/utils/utils.py
+++ astartes/utils/utils.py
@@ -3,16 +3,17 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from astartes import train_val_test_split
 
 
-def generate_regression_results_dict(sklearn_model,
-                                     X,
-                                     y,
-                                     samplers=["random"],
-                                     random_state=0,
-                                     samplers_hopts={},
-                                     train_size=0.8,
-                                     val_size=0.1,
-                                     test_size=0.1,
-                                     ):
+def generate_regression_results_dict(
+    sklearn_model,
+    X,
+    y,
+    samplers=["random"],
+    random_state=0,
+    samplers_hopts={},
+    train_size=0.8,
+    val_size=0.1,
+    test_size=0.1,
+):
     """
     Helper function to train a sklearn model using the provided data
     and provided sampler types.
@@ -25,11 +26,11 @@ def generate_regression_results_dict(sklearn_model,
         test_size (float, optional): Fraction of dataset to use in test set. Defaults to 0.1.
         random_state (int, optional): The random seed used throughout astartes.
         samplers_hopts (dict, optional): Should be a dictionary of dictionaries with the keys specifying
-                                         the sampler and the values being another dictionary with the 
+                                         the sampler and the values being another dictionary with the
                                          corresponding hyperparameters. Defaults to {}.
 
     Returns:
-        dict: nested dictionary with the format of 
+        dict: nested dictionary with the format of
             {
                 sampler: {
                     'mae':{
@@ -52,30 +53,35 @@ def generate_regression_results_dict(sklearn_model,
     """
     final_dict = {}
     for sampler in samplers:
-        error_dict = {'mae': {'train': [],
-                              'val': [],
-                              'test': [],
-                             },
-                      'rmse': {'train': [],
-                               'val': [],
-                               'test': [],
-                              },
-                      'R2': {'train': [],
-                               'val': [],
-                               'test': [],
-                            },
-                     }
+        error_dict = {
+            "mae": {
+                "train": [],
+                "val": [],
+                "test": [],
+            },
+            "rmse": {
+                "train": [],
+                "val": [],
+                "test": [],
+            },
+            "R2": {
+                "train": [],
+                "val": [],
+                "test": [],
+            },
+        }
 
         # obtain indices
-        _,_,_, train_indices, val_indices, test_indices = train_val_test_split(X,
-                                                                               train_size=train_size,
-                                                                               val_size=val_size,
-                                                                               test_size=test_size,
-                                                                               sampler=sampler,
-                                                                               random_state=random_state,
-                                                                               hopts=samplers_hopts.get(sampler, dict()),
-                                                                               return_indices=True,
-                                                                               )
+        _, _, _, train_indices, val_indices, test_indices = train_val_test_split(
+            X,
+            train_size=train_size,
+            val_size=val_size,
+            test_size=test_size,
+            sampler=sampler,
+            random_state=random_state,
+            hopts=samplers_hopts.get(sampler, dict()),
+            return_indices=True,
+        )
 
         # create data splits
         X_train = X[train_indices]
@@ -86,7 +92,6 @@ def generate_regression_results_dict(sklearn_model,
         y_val = y[val_indices]
         y_test = y[test_indices]
 
-
         # fit the model to the training data
         sklearn_model.fit(X_train, y_train)
 
@@ -95,36 +100,35 @@ def generate_regression_results_dict(sklearn_model,
         y_pred_val = sklearn_model.predict(X_val)
         y_pred_test = sklearn_model.predict(X_test)
 
-
         # store MAEs
         train_mae = mean_absolute_error(y_train, y_pred_train)
-        error_dict['mae']['train'].append(train_mae)
+        error_dict["mae"]["train"].append(train_mae)
 
         val_mae = mean_absolute_error(y_val, y_pred_val)
-        error_dict['mae']['val'].append(val_mae)
+        error_dict["mae"]["val"].append(val_mae)
 
         test_mae = mean_absolute_error(y_test, y_pred_test)
-        error_dict['mae']['test'].append(test_mae)
+        error_dict["mae"]["test"].append(test_mae)
 
         # store RMSEs
         train_rmse = mean_squared_error(y_train, y_pred_train, squared=False)
-        error_dict['rmse']['train'].append(train_rmse)
+        error_dict["rmse"]["train"].append(train_rmse)
 
         val_rmse = mean_squared_error(y_val, y_pred_val, squared=False)
-        error_dict['rmse']['val'].append(val_rmse)
+        error_dict["rmse"]["val"].append(val_rmse)
 
         test_rmse = mean_squared_error(y_test, y_pred_test, squared=False)
-        error_dict['rmse']['test'].append(test_rmse)
+        error_dict["rmse"]["test"].append(test_rmse)
 
         # store R2
         train_R2 = r2_score(y_train, y_pred_train)
-        error_dict['R2']['train'].append(train_R2)
+        error_dict["R2"]["train"].append(train_R2)
 
         val_R2 = r2_score(y_val, y_pred_val)
-        error_dict['R2']['val'].append(val_R2)
+        error_dict["R2"]["val"].append(val_R2)
 
         test_R2 = r2_score(y_test, y_pred_test)
-        error_dict['R2']['test'].append(test_R2)
+        error_dict["R2"]["test"].append(test_R2)
 
         final_dict[sampler] = error_dict
 
--- test/unit/utils/test_utils.py
+++ test/unit/utils/test_utils.py
@@ -19,29 +19,50 @@ class Test_utils(unittest.TestCase):
             [
                 [-0.86, -0.98],
                 [-0.42, -0.87],
-                [ 1.33,  0.20],
-                [-0.25,  2.43],
+                [1.33, 0.20],
+                [-0.25, 2.43],
                 [-0.59, -0.91],
-                [-0.33,  0.19],
+                [-0.33, 0.19],
                 [-0.10, -0.01],
-                [ 1.86,  1.15],
-                [ 0.64, -1.51],
-                [-0.36,  0.06],
-                [ 0.6 , -0.36],
-                [ 1.56, -0.09],
+                [1.86, 1.15],
+                [0.64, -1.51],
+                [-0.36, 0.06],
+                [0.6, -0.36],
+                [1.56, -0.09],
                 [-0.70, -1.66],
-                [-0.33,  0.44],
-                [ 1.58,  0.11],
-                [ 0.25, -0.05],
-                [-0.63,  0.79],
-                [-0.11,  0.00],
+                [-0.33, 0.44],
+                [1.58, 0.11],
+                [0.25, -0.05],
+                [-0.63, 0.79],
+                [-0.11, 0.00],
                 [-0.20, -1.19],
-                [ 0.71,  1.00],
+                [0.71, 1.00],
             ]
         )
-        self.y = np.array([-10.27, -6.19, 12.13, 4.90 , -7.77, -2.31, -0.89,
-                           19.42, 1.18, -2.97, 4.18, 13.26, -10.90 ,  -1.58,
-                           14.01, 2.00, -3.16, -0.91, -5.25, 9.07])
+        self.y = np.array(
+            [
+                -10.27,
+                -6.19,
+                12.13,
+                4.90,
+                -7.77,
+                -2.31,
+                -0.89,
+                19.42,
+                1.18,
+                -2.97,
+                4.18,
+                13.26,
+                -10.90,
+                -1.58,
+                14.01,
+                2.00,
+                -3.16,
+                -0.91,
+                -5.25,
+                9.07,
+            ]
+        )
 
     def test_generate_regression_results_dict(self):
         """Generate results dictionary for simple regression task."""
@@ -49,25 +70,33 @@ class Test_utils(unittest.TestCase):
         # use default hyperparameters
         sklearn_model = LinearSVR()
 
-        results_dict = generate_regression_results_dict(sklearn_model, self.X, self.y,
-                                                        train_size=0.6,
-                                                        val_size=0.2,
-                                                        test_size=0.2,
-                                                        )
-        
+        results_dict = generate_regression_results_dict(
+            sklearn_model,
+            self.X,
+            self.y,
+            train_size=0.6,
+            val_size=0.2,
+            test_size=0.2,
+        )
+
         # test that only results for the default random sampler are included
-        self.assertEqual(len(results_dict), 1,
-                         msg=f"results_dict contained {results_dict.keys()}. Expected just random sampler.",
-                         )
+        self.assertEqual(
+            len(results_dict),
+            1,
+            msg=f"results_dict contained {results_dict.keys()}. Expected just random sampler.",
+        )
         # test that results for mae, rmse, and r2 are included
-        self.assertTrue('mae' in results_dict['random'].keys(),
-                        msg=f"results_dict did not contain MAE results.",
+        self.assertTrue(
+            "mae" in results_dict["random"].keys(),
+            msg=f"results_dict did not contain MAE results.",
         )
-        self.assertTrue('rmse' in results_dict['random'].keys(),
-                        msg=f"results_dict did not contain RMSE results.",
+        self.assertTrue(
+            "rmse" in results_dict["random"].keys(),
+            msg=f"results_dict did not contain RMSE results.",
         )
-        self.assertTrue('R2' in results_dict['random'].keys(),
-                        msg=f"results_dict did not contain R2 results.",
+        self.assertTrue(
+            "R2" in results_dict["random"].keys(),
+            msg=f"results_dict did not contain R2 results.",
         )
 
 
