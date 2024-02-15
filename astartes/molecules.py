3b8bca1962b0597513f288ea4d6f59c95aae4532
Jackson Burns <33505528+JacksonBurns@users.noreply.github.com>
2024-02-15 13:49:21 -0500

    abstract the aimsim featurization function to utils

--- astartes/molecules.py
+++ astartes/molecules.py
@@ -1,30 +1,12 @@
-import warnings
-
 import numpy as np
 
-from astartes.utils.exceptions import MoleculesNotInstalledError
-
-try:
-    """
-    aimsim depends on sklearn_extra, which uses a version checking technique that is due to
-    be deprecated in a version of Python after 3.11, so it is throwing a deprecation warning
-    We ignore this warning since we can't do anything about it (sklearn_extra seems to be
-    abandonware) and in the future it will become an error that we can deal with.
-    """
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore", category=DeprecationWarning)
-        from aimsim.chemical_datastructures import Molecule
-        from aimsim.exceptions import LoadingError
-except ImportError:  # pragma: no cover
-    raise MoleculesNotInstalledError(
-        """To use molecule featurizer, install astartes with pip install astartes[molecules]."""
-    )
 
 # at this point we have successfully verified that rdkit is installed, so we can do this:
 from rdkit.rdBase import SeedRandomNumberGenerator
 
 from astartes import train_test_split, train_val_test_split
 from astartes.main import DEFAULT_RANDOM_STATE
+from astartes.utils.aimsim_featurizer import featurize_molecules
 
 SeedRandomNumberGenerator(DEFAULT_RANDOM_STATE)
 
@@ -65,7 +47,7 @@ def train_val_test_split_molecules(
     if sampler == "scaffold":
         X = molecules
     else:
-        X = _featurize(molecules, fingerprint, fprints_hopts)
+        X = featurize_molecules(molecules, fingerprint, fprints_hopts)
     return train_val_test_split(
         X,
         y=y,
@@ -115,7 +97,7 @@ def train_test_split_molecules(
     if sampler == "scaffold":
         X = molecules
     else:
-        X = _featurize(molecules, fingerprint, fprints_hopts)
+        X = featurize_molecules(molecules, fingerprint, fprints_hopts)
 
     # call train test split with this input
     return train_test_split(
@@ -129,37 +111,3 @@ def train_test_split_molecules(
         hopts=hopts,
         return_indices=return_indices,
     )
-
-
-def _featurize(molecules, fingerprint, fprints_hopts):
-    """Call AIMSim's Molecule to featurize the molecules according to the arguments.
-
-    Args:
-        molecules (np.array): SMILES strings or RDKit molecule objects.
-        fingerprint (str): The molecular fingerprint to be used.
-        fprints_hopts (dict): Hyperparameters for AIMSim.
-
-    Returns:
-        np.array: X array (featurized molecules)
-    """
-    X = []
-    for molecule in molecules:
-        try:
-            if type(molecule) in (np.str_, str):
-                mol = Molecule(mol_smiles=molecule)
-            else:
-                mol = Molecule(mol_graph=molecule)
-        except LoadingError as le:
-            raise RuntimeError(
-                "Unable to featurize molecules using '{:s}' with this configuration: fprint_hopts={:s}"
-                "\nCheck terminal output for messages from the RDkit logger. ".format(
-                    fingerprint, repr(fprints_hopts)
-                )
-            ) from le
-        mol.descriptor.make_fingerprint(
-            mol.mol_graph,
-            fingerprint,
-            fingerprint_params=fprints_hopts,
-        )
-        X.append(mol.descriptor.to_numpy())
-    return np.array(X)
---
+++ astartes/utils/aimsim_featurizer.py
@@ -0,0 +1,51 @@
+import warnings
+
+import numpy as np
+
+from astartes.utils.exceptions import MoleculesNotInstalledError
+
+try:
+    """
+    aimsim depends on sklearn_extra, which uses a version checking technique that is due to
+    be deprecated in a version of Python after 3.11, so it is throwing a deprecation warning
+    We ignore this warning since we can't do anything about it (sklearn_extra seems to be
+    abandonware) and in the future it will become an error that we can deal with.
+    """
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", category=DeprecationWarning)
+        from aimsim.chemical_datastructures import Molecule
+        from aimsim.exceptions import LoadingError
+except ImportError:  # pragma: no cover
+    raise MoleculesNotInstalledError("""To use molecule featurizer, install astartes with pip install astartes[molecules].""")
+
+
+def featurize_molecules(molecules, fingerprint, fprints_hopts):
+    """Call AIMSim's Molecule to featurize the molecules according to the arguments.
+
+    Args:
+        molecules (np.array): SMILES strings or RDKit molecule objects.
+        fingerprint (str): The molecular fingerprint to be used.
+        fprints_hopts (dict): Hyperparameters for AIMSim.
+
+    Returns:
+        np.array: X array (featurized molecules)
+    """
+    X = []
+    for molecule in molecules:
+        try:
+            if type(molecule) in (np.str_, str):
+                mol = Molecule(mol_smiles=molecule)
+            else:
+                mol = Molecule(mol_graph=molecule)
+        except LoadingError as le:
+            raise RuntimeError(
+                "Unable to featurize molecules using '{:s}' with this configuration: fprint_hopts={:s}"
+                "\nCheck terminal output for messages from the RDkit logger. ".format(fingerprint, repr(fprints_hopts))
+            ) from le
+        mol.descriptor.make_fingerprint(
+            mol.mol_graph,
+            fingerprint,
+            fingerprint_params=fprints_hopts,
+        )
+        X.append(mol.descriptor.to_numpy())
+    return np.array(X)
